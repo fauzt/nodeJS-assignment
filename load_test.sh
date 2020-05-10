0e21b0df0fee58b816d64aaef49c798961ce445d
fauzt <fauzan.a.y@gmail.com>
2020-05-10 14:49:07 +0800

    updated load tests parameters

--- load_test.sh
+++ load_test.sh
@@ -1,3 +1,3 @@
-loadtest -t 30 -c 10 --rps 100 http://localhost:8080/
-loadtest -t 30 -c 10 --rps 500 http://localhost:8080/
-loadtest -t 30 -c 10 --rps 1000 http://localhost:8080/
+loadtest -t 30 -c 10 --rps 100 http://localhost:8080/py/mult?x=5&y=25
+loadtest -t 30 -c 10 --rps 500 http://localhost:8080/py/mult?x=5&y=25
+loadtest -t 30 -c 10 --rps 1000 http://localhost:8080/py/mult?x=5&y=25
